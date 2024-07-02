a162e78fc54c60aa965199587bdcc519351fbbfb
fdhhhdjd <nguyentientai10@gmail.com>
2024-07-02 14:35:36 +0700

    feat: rabbitmq  queue naming convention

--- src/helpers/messageQueueHelpers.js
+++ src/helpers/messageQueueHelpers.js
@@ -3,6 +3,16 @@ const {
 } = require('@/constants');
 const { getNodeApp } = require('./appHelpers');
 
+/**
+ * Generates a queue name based on the provided parameters.
+ *
+ * @param {Object} options - The options object.
+ * @param {string} [options.app=MY_APP] - The app name.
+ * @param {string} [options.env=getNodeApp()] - The environment.
+ * @param {string} [options.feature=ANONYMS] - The feature.
+ * @param {string} [options.action=SEND] - The action.
+ * @returns {string} The generated queue name.
+ */
 const generateQueueName = ({
   app = MY_APP,
   env = getNodeApp(),
